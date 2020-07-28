3fb86f12f95544988a88b2ef481144ac0e11de8e
TheBrokas <dinosaurpinkalicious@gmail.com>
2020-07-27 20:51:01 -0400

    Nicer Graphs

--- GraphGeneration.py
+++ GraphGeneration.py
@@ -22,14 +22,18 @@ def generate_daily_graph(user_id,stat,user_name):
         daily_data = json.load(json_file)
     data = daily_data[user_id][stat]
     plt.style.use('dark_background')
+    fig = plt.figure()
+    ax = plt.gca()
+    fig.patch.set_facecolor('#36393f')
+    ax.set_facecolor('#36393f')
     plt.plot(*zip(*sorted(data.items())), marker = 'o')
-    plot_title = user_name + ' ' + stat + ' daily graph'
+    plot_title = user_name + ': ' + stat + ' -DailyGraph'
     plt.title(plot_title)
     plt.xlabel('DAY')
     plt.ylabel(stat.upper())
     plt.ylim(ymin=0)
     file_location = str(user_id) + '_' + str(stat) + '_graph.png'
-    plt.savefig(file_location)
+    plt.savefig(file_location, facecolor=fig.get_facecolor(),edgecolor='none')
     return file_location, plt.figure()
-#generate_daily_graph('90559730715467776','kills','mtb')
+generate_daily_graph('90559730715467776','kills','mtb')
 
