e683a91aa58cf77989221dc3006de073b8600ce2
TheBrokas <dinosaurpinkalicious@gmail.com>
2020-07-29 17:22:47 -0400

    Further code cleanup, but removing amtch still broken

--- StatBot.py
+++ StatBot.py
@@ -23,52 +23,31 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     if message.author == client.user: return
-
     if message.content.startswith('!addmatch'):
         if path.exists("StatBotInfo.txt") == True:
             with open('StatBotInfo.txt') as json_file:
                 data = json.load(json_file)
-            user_message = message.content
-            try:
-                prefix,match_info=user_message.split(' ')
-            except:
-                await message.channel.send('Invalid input, try again.')
-            else:
-                userid,username = message_username_userid(message)
-                try:
-                    outcome,kda = match_info.split('-')
-                except:
-                    await message.channel.send('Invalid input, try again.')
+                try: kill,death,assists,outcome,userid,username = split_user_input(message)
+                except: await message.channel.send('Invalid input, try again.')
+                if str(userid) in data:
+                    data[userid]['username'] = re.sub(r'\W+', '', message.author.name)
+                    data[userid]['games'] += 1
+                    update_stats_from_input(data,userid,kill,death,assists,outcome)
                 else:
-                    try:
-                        kill_str,death_str,assists_str = kda.split('/')
-                    except:
-                        await message.channel.send('Invalid input, try again.')
-                    if kill_str.isnumeric() and death_str.isnumeric() and assists_str.isnumeric():
-                        kill = abs(int(kill_str))
-                        death = abs(int(death_str))
-                        assists = abs(int(assists_str))
-                        if str(userid) in data:
-                            data[userid]['username'] = re.sub(r'\W+', '', message.author.name)
-                            data[userid]['games'] += 1
-                            update_stats_from_input(data,userid,kill,death,assists,outcome)
-                        else:
-                            data[userid] = {
-                                'username': re.sub(r'\W+', '', message.author.name),
-                                'games': 1,
-                                'wins': 0,
-                                'losses': 0,
-                                'ties': 0,
-                                'kills': [],
-                                'deaths': [],
-                                'assists': [],
-                            }
-                            update_stats_from_input(data,userid,kill,death,assists,outcome)
-                        await message.channel.send('Match Added.')
-                        with open('StatBotInfo.txt', 'w', encoding='utf-8') as f:
-                            json.dump(data, f, ensure_ascii=False, indent=4)
-                    else:
-                        await message.channel.send('Invalid formating, try again.')
+                    data[userid] = {
+                        'username': re.sub(r'\W+', '', message.author.name),
+                        'games': 1,
+                        'wins': 0,
+                        'losses': 0,
+                        'ties': 0,
+                        'kills': [],
+                        'deaths': [],
+                        'assists': [],
+                    }
+                    update_stats_from_input(data,userid,kill,death,assists,outcome)
+                await message.channel.send('Match Added.')
+                with open('StatBotInfo.txt', 'w', encoding='utf-8') as f:
+                    json.dump(data, f, ensure_ascii=False, indent=4)
         else:
             data = {
              }
@@ -123,6 +102,13 @@ async def on_message(message):
         else:
             await message.channel.send('Invalid request. You are can request: wins, losses, ties, kills, deaths, assists, KD, winrate.')
 
+    if message.content.startswith('!delmatch'):
+        try: kill,death,assists,outcome,userid,username = split_user_input(message)
+        except: await message.channel.send('Invalid input, try again.')
+        else:
+            remove_matchadd(userid,kill,death,assists,outcome)
+
+
 def stat_calculator(names,kills,deaths,games,wins):
         name = names
         total_kills = kills
@@ -139,7 +125,7 @@ def stat_calculator(names,kills,deaths,games,wins):
             kd = total_kills
         return name,win_rate,kd,total_games
 
-def update_stats_from_input(data,userid,kills,deaths,assists,game_outcome):
+def update_stats_from_input(data,userid,kills,deaths,assists,game_outcome): #Updates the stats from the user input. 
     data[userid]['kills'].append(kills)
     data[userid]['deaths'].append(deaths)
     data[userid]['assists'].append(assists)
@@ -147,9 +133,33 @@ def update_stats_from_input(data,userid,kills,deaths,assists,game_outcome):
     if game_outcome.upper() == 'L':data[userid]['losses'] += 1
     if game_outcome.upper() == 'T':data[userid]['ties'] += 1
 
-def message_username_userid(message):
-    userid = message.author.id
+def message_username_userid(message): #Finds the messangers username and userid
+    userid = str(message.author.id)
     username = re.sub(r'\W+', '', message.author.name)
     return userid,username
+
+def remove_matchadd(userid,kill,death,assist,game_outcome): #Removes the most recently added match
+    with open('StatBotInfo.txt') as json_file:
+        data = json.load(json_file)
+        data[userid]['kills'].pop()
+        data[userid]['deaths'].remove(death)
+        data[userid]['assists'].remove(assist)
+        if game_outcome.upper() == 'W':data[userid]['wins'] -= 1
+        if game_outcome.upper() == 'L':data[userid]['losses'] -= 1
+        if game_outcome.upper() == 'T':data[userid]['ties'] -= 1
+        print("removing match")
+        print("kill:")
+        print(kill)
+
+def split_user_input(message): #Takes the user input and divides it into corresponding variables.
+    user_message = message.content
+    prefix,match_info=user_message.split(' ')
+    userid,username = message_username_userid(message)
+    outcome,kda = match_info.split('-')
+    kill_str,death_str,assists_str = kda.split('/')
+    kill = abs(int(kill_str))
+    death = abs(int(death_str))
+    assists = abs(int(assists_str))
+    return kill,death,assists,outcome,userid,username
 
 client.run(TOKEN)
